2614f1446d6aefa2dd486dbe473778a467d24ae4
Davide Garberi <dade.garberi@gmail.com>
2023-06-09 16:46:52 +0200

    realme_trinket: Switch to source built libplatformconfig
    Change-Id: Ifa806fe4ae2981f0ad98193b62a37c7e9d8c1d50

--- realme_trinket-vendor.mk
+++ realme_trinket-vendor.mk
@@ -3402,7 +3402,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libantradio \
     libMpeg4SwEncoder \
-    libplatformconfig \
     libthermalclient \
     com.quicinc.cne.api@1.0 \
     com.quicinc.cne.api@1.1 \
